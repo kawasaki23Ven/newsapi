d98f2cd1f1df4f0548df4e9a0c42ebe6fd6f007c
kawasaki23ven <mendozaanelsy2@gmail.com>
2020-04-27 20:21:55 -0500

    Solucionado carga del logo de react en home

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <Grid container justify="center">
             <Grid item md={6} sm={12} justify="center" style={{ textAlign: "center" }}>
-                <img src="logo512.png" style={{ maxWidth: "100%" }} alt="logo" />
+                <img src="./logo512.png" style={{ maxWidth: "100%" }} alt="logo" />
             </Grid>
         </Grid>
     )
